324a95b68a915f07cf7faa0c688dd156fd9856e7
VitalyKuzminsky <vitaly-kuzminsky@mail.ru>
2022-09-09 00:47:20 +0300

    ДЗ к уроку 4

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -13,9 +13,22 @@
 """
 
 
+from timeit import timeit
+
+
 def func_1(nums):
     new_arr = []
     for i in range(len(nums)):
         if nums[i] % 2 == 0:
             new_arr.append(i)
     return new_arr
+
+
+def func_2(nums):
+    return [i for i in range(len(nums)) if nums[i] % 2 == 0]
+
+
+t1 = timeit('func_1', globals=globals())  # -> 0.04465719999279827
+t2 = timeit('func_2', globals=globals())  # -> 0.026877099997363985
+print(f'функция func_2 на {(100 - (t2 * 100 / t1)):.2f}% быстрее, чем func_1')
+# list comprehension выполняется быстрее, чем перебор элементов
--- Урок 4. Практическое задание/task_3.py
+++ Урок 4. Практическое задание/task_3.py
@@ -13,14 +13,18 @@
 """
 
 
-def revers(enter_num, revers_num=0):
+from timeit import timeit
+from cProfile import run
+
+
+def revers_1(enter_num, revers_num=0):
     if enter_num == 0:
         return
     else:
         num = enter_num % 10
         revers_num = (revers_num + num / 10) * 10
         enter_num //= 10
-        revers(enter_num, revers_num)
+        revers_1(enter_num, revers_num)
 
 
 def revers_2(enter_num, revers_num=0):
@@ -35,3 +39,34 @@ def revers_3(enter_num):
     enter_num = str(enter_num)
     revers_num = enter_num[::-1]
     return revers_num
+
+
+def revers_4(enter_num):
+    return ''.join(reversed(str(enter_num)))
+
+
+def time_check(number_in: int, *args):
+    times = {}
+    for el in args:
+        times[el] = timeit(f'{el}({number_in})', globals=globals())
+    for key, value in times.items():
+        print(f'Время выполнения функции {key}: {value}')
+
+
+number = 4789
+time_check(number, 'revers_1', 'revers_2', 'revers_3', 'revers_4')
+print()
+run('revers_1(number)')
+run('revers_2(number)')
+run('revers_3(number)')
+run('revers_4(number)')
+
+"""
+Время выполнения функции revers_1: 1.6248673999216408
+Время выполнения функции revers_2: 1.0988760999171063
+Время выполнения функции revers_3: 0.37664820009376854
+Время выполнения функции revers_4: 0.6481388999382034
+
+Самым шустырм оказался срез, за ним встроенная функция реверса,
+а так как в рекурсии и цикле есть расчёты, поэтому они проигрывают в 2-5 раз.
+"""
